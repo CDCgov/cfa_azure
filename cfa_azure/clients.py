97458067cf28d3d507bfb7a21f823e93333af632
Ryan Raasch <xng3@cdc.gov>
2024-05-14 09:55:07 -0700

    change parameter name in list_blobs_flat

--- cfa_azure/clients.py
+++ cfa_azure/clients.py
@@ -712,10 +712,10 @@ class AzureClient:
             print("Please specify a blob container or have mounts associated with the client.")
             return None
         if blob_container:
-            filenames = helpers.list_blobs_flat(blob_container = blob_container, blob_service_container = self.blob_service_client, verbose = False)
+            filenames = helpers.list_blobs_flat(container_name = blob_container, blob_service_container = self.blob_service_client, verbose = False)
         elif self.mounts:
             filenames = []
             for mount in self.mounts:
-                _files = helpers.list_blobs_flat(blob_container = mount[0], blob_service_container = self.blob_service_client, verbose = False)
+                _files = helpers.list_blobs_flat(container_name = mount[0], blob_service_container = self.blob_service_client, verbose = False)
                 filenames += _files
         return filenames

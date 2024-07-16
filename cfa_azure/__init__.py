8b33f5dabd180ea71703e9439f37a275aa04db1b
Ryan Raasch <xng3@cdc.gov>
2024-07-16 21:09:06 +0000

    move logger to __init__

--- cfa_azure/__init__.py
+++ cfa_azure/__init__.py
@@ -1,1 +1,26 @@
+import logging
+import datetime
+import os
+import sys
+from cfa_azure import helpers
+
 __all__ = ["batch", "helpers", "clients"]
+
+logger = logging.getLogger(__name__)
+run_time = datetime.datetime.now()
+now_string = f"{run_time:%Y-%m-%d_%H:%M:%S%z}"
+# Logging
+if not os.path.exists("logs"):
+    os.mkdir("logs")
+logfile = os.path.join("logs", f"{now_string}.log")
+FORMAT = "[%(levelname)s] %(asctime)s: %(message)s"
+
+logging.basicConfig(
+    level=helpers.get_log_level(),
+    format=FORMAT,
+    datefmt="%Y-%m-%d_%H:%M:%S%z",
+    handlers=[
+        logging.StreamHandler(sys.stdout),
+        logging.FileHandler(logfile),
+    ],
+)
--- cfa_azure/clients.py
+++ cfa_azure/clients.py
@@ -9,23 +9,6 @@ from azure.core.exceptions import HttpResponseError
 from cfa_azure import helpers
 
 logger = logging.getLogger(__name__)
-run_time = datetime.datetime.now()
-now_string = f"{run_time:%Y-%m-%d_%H:%M:%S%z}"
-# Logging
-if not exists("logs"):
-    os.mkdir("logs")
-logfile = join("logs", f"{now_string}.log")
-FORMAT = "[%(levelname)s] %(asctime)s: %(message)s"
-
-logging.basicConfig(
-    level=helpers.get_log_level(),
-    format=FORMAT,
-    datefmt="%Y-%m-%d_%H:%M:%S%z",
-    handlers=[
-        logging.StreamHandler(sys.stdout),
-        logging.FileHandler(logfile),
-    ],
-)
 
 class AzureClient:
     def __init__(self, config_path: str):

7c88e53065a3a2e84b33a7eecf9fe65bbe1fbbea
Ryan Raasch <xng3@cdc.gov>
2024-07-23 18:25:43 +0000

    fix logic

--- cfa_azure/__init__.py
+++ cfa_azure/__init__.py
@@ -18,12 +18,17 @@ logfile = os.path.join("logs", f"{now_string}.log")
 FORMAT = "[%(levelname)s] %(asctime)s: %(message)s"
 
 log_status = os.getenv("LOG_OUTPUT")
-if log_status is None or log_status.lower().startswith ("both"):
+if log_status is None:
     handler = [
         logging.StreamHandler(sys.stdout),
         logging.FileHandler(logfile)
     ]
-if log_status.lower().startswith("file"):
+elif log_status.lower().startswith ("both"):
+    handler = [
+        logging.StreamHandler(sys.stdout),
+        logging.FileHandler(logfile)
+    ]
+elif log_status.lower().startswith("file"):
     handler = [
         logging.FileHandler(logfile)
     ]

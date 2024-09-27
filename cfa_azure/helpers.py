31e3c47afc64b169555d8b3277446fe78a138696
Fawad Rafi <xop5@cdc.gov>
2024-09-27 17:12:50 +0000

    Added instructions and code for changing containers on existing pool

--- cfa_azure/helpers.py
+++ cfa_azure/helpers.py
@@ -18,6 +18,7 @@ from azure.batch import BatchServiceClient
 from azure.batch.models import JobConstraints
 from azure.batch.models import ComputeNodeListOptions
 from azure.batch.models import JobConstraints
+from azure.batch.models import ComputeNodeListOptions
 from azure.common.credentials import ServicePrincipalCredentials
 from azure.containerregistry import ContainerRegistryClient
 from azure.core.exceptions import HttpResponseError

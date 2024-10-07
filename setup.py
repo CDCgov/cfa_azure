2af4717e10aee5d02b2cf26854a10108ce7a5db0
Ryan Raasch <150935395+ryanraaschCDC@users.noreply.github.com>
2024-10-07 14:56:38 -0700

    Task name: ability to add a suffix to task name (#150)
    * add task name option to include a suffix
    
    * bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="cfa_azure",
-    version="1.0.4",
+    version="1.0.5",
     description="module for use with Azure and Azure Batch",
     packages=find_packages(exclude=["tests", "venv"]),
     author="Ryan Raasch",

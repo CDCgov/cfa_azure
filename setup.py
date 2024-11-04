509708ee0d45a539e3b179aeb0b53d1e799b95b1
Ryan Raasch <xng3@cdc.gov>
2024-11-04 22:52:11 +0000

    update packages

--- setup.py
+++ setup.py
@@ -8,14 +8,16 @@ setup(
     author="Ryan Raasch",
     author_email="xng3@cdc.gov",
     install_requires=[
-        "azure-identity==1.16.1",
-        "azure-keyvault==4.2.0",
-        "azure-batch==14.0.0",
-        "azure-mgmt-batch==17.1.0",
-        "azure-storage-blob==12.17.0",
-        "azure-containerregistry==1.2.0",
+        "azure-identity>=1.16.1",
+        "azure-keyvault>=4.2.0",
+        "azure-batch>=14.0.0",
+        "azure-mgmt-batch>=17.1.0",
+        "azure-mgmt-resource>=23.2.0",
+        "azure-mgmt-authorization>=4.0.0"
+        "azure-storage-blob>=12.17.0",
+        "azure-containerregistry>=1.2.0",
         "cryptography>=43.0.1"
-        "toml==0.10.2",
+        "toml>=0.10.2",
         "pandas",
         "pathlib",
         "docker",

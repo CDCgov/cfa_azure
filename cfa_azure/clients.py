1349626418e0b1199d2f378084b26e2070efb446
Fawad Rafi <xop5@cdc.gov>
2025-01-16 04:46:46 +0000

    Linting issues fixed

--- cfa_azure/clients.py
+++ cfa_azure/clients.py
@@ -1239,11 +1239,7 @@ class AzureClient:
         """
         # monitor the tasks
         logger.debug(f"starting to monitor job {job_id}.")
-        monitor = helpers.monitor_tasks(
-            job_id,
-            timeout,
-            self.batch_client
-        )
+        monitor = helpers.monitor_tasks(job_id, timeout, self.batch_client)
         print(monitor)
 
         # delete job automatically if debug is false

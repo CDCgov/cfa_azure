3097ae5154d064a9724710999702d80dd3bf0e56
Fawad Rafi <xop5@cdc.gov>
2025-03-03 16:36:30 +0000

    Bumped up version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="cfa_azure",
-    version="1.3.4",
+    version="1.3.6",
     description="module for use with Azure and Azure Batch",
     packages=find_packages(exclude=["tests", "venv"]),
     author="Ryan Raasch",
@@ -23,7 +23,5 @@ setup(
         "polars",
         "pyyaml",
         "griddler @ git+https://github.com/CDCgov/pygriddler.git@main#egg=griddler",
-        "adlfs",
-        "fsspec",
     ],
 )

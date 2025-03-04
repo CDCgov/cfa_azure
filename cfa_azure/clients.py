dfbc1da3e2abc7d1ab7980708be29969a544d5e1
Ryan Raasch <xng3@cdc.gov>
2025-03-04 15:19:16 +0000

    update doc string for new method

--- cfa_azure/clients.py
+++ cfa_azure/clients.py
@@ -1709,6 +1709,15 @@ class AzureClient:
         container_name: str,
         **kwargs,
     ):
+        """
+        download specified blob files to be downloaded after a job completes
+
+        Args:
+            job_id (str): name of job to monitor
+            blob_paths (list[str]): list of blob paths for directories/files to download
+            target (str): local folder path for where to store downloaded objects
+            container_name (str): Blob container name where objects will live
+        """
         # check job for completion
         helpers.monitor_tasks(
             job_id=job_id, timeout=None, batch_client=self.batch_client

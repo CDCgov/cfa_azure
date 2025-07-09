a2eb2fde66f223a611d7288f994030b0464989d4
xop5 <xop5@cdc.gov>
2025-07-09 10:25:32 -0400

    Regenerated the Azure batch decorator as a callable class

--- examples/metaflow/main.py
+++ examples/metaflow/main.py
@@ -1,6 +1,17 @@
 from metaflow import FlowSpec, step
+import os
+import sys
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+plugins_folder = os.path.join(current_dir, "custom_metaflow", "plugins", "decorators")
+if plugins_folder not in sys.path:
+    sys.path.insert(0, plugins_folder)
+
+print(sys.path)
+
 from custom_metaflow.plugins.decorators.cfa_azure_batch_decorator import CFAAzureBatchDecorator
 
+cfa_azure_batch = CFAAzureBatchDecorator(config_file="client_config.toml")
 
 class MyFlow(FlowSpec):
     @step
@@ -9,7 +20,7 @@ class MyFlow(FlowSpec):
         self.next(self.foo)
 
     @step
-    @CFAAzureBatchDecorator(config_file="client_config.toml")
+    @cfa_azure_batch(config_file="client_config.toml")
     def foo(self):
         print("Running the foo step in Azure Batch...")
         #from cfa_azure.clients import AzureClient
